7e2269af600193282bffb545c02f71781bde74b4
JuanCruzEDominguez <dominguez.juanelias@gmail.com>
2023-10-29 22:13:29 -0300

    Intento de Implementación de Bing maps Api

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,9 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
+
+/*Menú Hamburguesa*/
+
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
@@ -9,6 +12,9 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+/*Preguntas Frecuentes Colapsable */
 
 var faq = document.getElementsByClassName("question");
 var i;
@@ -24,3 +30,16 @@ for (i = 0; i < faq.length; i++) {
         }
     });
 } 
+
+
+var map;
+
+function getmap(){
+    map.entities.clear
+    map = New.Microsoft.Map(document.getelemntbyid("map"), {
+        credentials:'Amst3HQKXPmTU8-EHBCSri7fBKM8JE5RT3JXWWxtthGlLLct7GGrOQWYOig5VtwP',
+        center: new Microsoft.Maps.Location(-34.61315, -58.37723),
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        zoom: 10,
+    });
+};
