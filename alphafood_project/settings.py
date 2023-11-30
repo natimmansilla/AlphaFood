0979a484f2dafbae452f2e3eb4711177e7a301ed
Paola Basualdo <paoladeembalse@gmail.com>
2023-11-30 00:33:05 +0000

    Redireccionar a mascotas/index.html

--- alphafood_project/settings.py
+++ alphafood_project/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+CUSTOM_APPS = ["mascotas_app"
+
+]
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -37,8 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "mascotas_app",
+    #"mascotas_app",
 ]
+INSTALLED_APPS += CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- mascotas_app/urls.py
+++ mascotas_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "mascotas_app"
 
 urlpatterns= [
-    path ('', views.inicio, name='inicio'), 
+    path ('', views.mascotas, name='inicio'), 
     path ('uno', views.uno, name='uno'),
     path ('mascotas', views.mascotas, name='mascotas'),
 ]
