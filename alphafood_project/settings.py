39611829b7d4f8996cf2ae0926cc6b18166606c9
Paola Basualdo <paoladeembalse@gmail.com>
2023-11-30 18:03:25 +0000

    conectar  a sqlite

--- alphafood_project/settings.py
+++ alphafood_project/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = "alphafood_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
--- mascotas_app/models.py
+++ mascotas_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Mascota(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    nombre = models.CharField(max_length=100, verbose_name="Nombre", null=False, blank=False)
+    edad = models.IntegerField(verbose_name="Edad", null=False, blank=False)
+    peso = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Peso", null=False, blank=False)
+    raza = models.CharField(max_length=100, verbose_name="Raza", null=False, blank=False)
+    email = models.EmailField(verbose_name="Email", null=False, blank=False)
+
+   
