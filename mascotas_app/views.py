fa9536f7d5484ede0d5b0419f1ac4d1c120a7168
Paola Basualdo <paoladeembalse@gmail.com>
2023-12-02 02:22:57 +0000

    Corregir CRUD

--- mascotas_app/views.py
+++ mascotas_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Mascota 
+from .forms import MascotaForm
 
 def inicio(request):
     return HttpResponse('<h1>REGISTRO DE MASCOTAS</H1>')
@@ -10,11 +12,21 @@ def uno(request):
 
 def mascotas(request):
     mascotas=Mascota.objects.all()
-    print(mascotas)
     return render (request, 'mascotas/index.html', {'mascotas':mascotas})
 
-def registrar(request):
-    return render(request, 'mascotas/registrar.html')
+def registrar(request): 
+    formulario= MascotaForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        mascota_nueva = formulario.save()
+        return redirect ('mascotas_app:mascotas')
+    return render(request, 'mascotas/registrar.html', {'formulario': formulario})
 
-def editar(request):
-    return render(request, 'mascotas/editar.html')
+def editar(request, id):
+    mascota=Mascota.objects.get(id=id)
+    formulario=MascotaForm(request.POST or None, request.FILES or None, instance=mascota)
+    return render(request, 'mascotas/editar.html', {'formulario': formulario})
+
+def eliminar(request, id):
+    mascota = Mascota.objects.get(id= id)
+    mascota.delete()
+    return redirect ('mascotas_app:mascotas')
