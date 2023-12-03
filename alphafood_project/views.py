55e193aaad0fc2544c67e0485741e1788e9b56ca
Paola Basualdo <paoladeembalse@gmail.com>
2023-12-03 04:44:37 +0000

    establecer urls para la barra de navegacion de mascotas_app

--- alphafood_project/views.py
+++ alphafood_project/views.py
@@ -1,5 +1,21 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class IndexPage(TemplateView):
     template_name = "index.html"
+
+def menus(request):
+    return render(request, 'index.html', {'scroll_to_id': 'menus'})
+
+def instrucciones(request):
+    return render(request, 'index.html', {'scroll_to_id': 'instrucciones'})
+
+def preguntas(request):
+    return render(request, 'index.html', {'scroll_to_id': 'preguntas'})
+
+def donde_estamos(request):
+    return render(request, 'index.html', {'scroll_to_id': 'dondeEstamos'})
+
+def contacto(request):
+    return render(request, 'index.html', {'scroll_to_id': 'contacto'})
     
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,37 @@
+import { scrollToTarget } from './scroll.js';
+/*const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+var faq = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+} 
+
+document.addEventListener('DOMContentLoaded', function() {
+    scrollToTarget('menus');
+
+    
+});*/
+
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
@@ -24,3 +58,17 @@ for (i = 0; i < faq.length; i++) {
         }
     });
 } 
+
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); 
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetElement = document.getElementById(targetId); 
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' }); 
+        }
+    });
+});
